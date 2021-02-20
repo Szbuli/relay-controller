15520d78d8cca15d31339fe0e304709c20058933
Bálint Száray <6738847+Szbuli@users.noreply.github.com>
2021-02-20 22:46:27 +0100

    fix auto discovery state

--- firmware/bluepill-can-relay-controller/Src/config/ha_auto_discovery.c
+++ firmware/bluepill-can-relay-controller/Src/config/ha_auto_discovery.c
@@ -9,20 +9,20 @@
 #include "can_support.h"
 #include "can-ids.h"
 
-void sendAutoDiscoveryMessage(uint16_t topicId, uint16_t stateTopicId, uint8_t data[]) {
+void sendAutoDiscoveryMessage(uint16_t stateTopicId, uint8_t data[]) {
 	data[4] = stateTopicId >> 8;
 	data[5] = stateTopicId;
-	putCanMessageToQueue(topicId, data, 8, CAN_RTR_DATA);
+	putCanMessageToQueue(RELAY_CONTROLLER_HA_SENSOR_CONFIG, data, 8, CAN_RTR_DATA);
 }
 
 void publishConfigForAutoDiscovery() {
-	if (homeConfig.listenForDeviceIdMode != 0) {
+	if (homeConfig.listenForDeviceIdMode == 0) {
 		uint8_t data[] = { VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, DEVICE_TYPE_ID_ALARM_CONTROLLER, 0, 0,
 		RELAY_CONTROLLER_HEARTBEAT >> 8, RELAY_CONTROLLER_HEARTBEAT };
-		sendAutoDiscoveryMessage(RELAY_CONTROLLER_HA_SENSOR_CONFIG, RELAY_CONTROLLER_SET_RELAY_1, data);
-		sendAutoDiscoveryMessage(RELAY_CONTROLLER_HA_SENSOR_CONFIG, RELAY_CONTROLLER_SET_RELAY_2, data);
-		sendAutoDiscoveryMessage(RELAY_CONTROLLER_HA_SENSOR_CONFIG, RELAY_CONTROLLER_SET_RELAY_3, data);
-		sendAutoDiscoveryMessage(RELAY_CONTROLLER_HA_SENSOR_CONFIG, RELAY_CONTROLLER_SET_RELAY_4, data);
-		sendAutoDiscoveryMessage(RELAY_CONTROLLER_HA_SENSOR_CONFIG, RELAY_CONTROLLER_SET_RELAY_5, data);
+		sendAutoDiscoveryMessage(RELAY_CONTROLLER_SET_RELAY_1, data);
+		sendAutoDiscoveryMessage(RELAY_CONTROLLER_SET_RELAY_2, data);
+		sendAutoDiscoveryMessage(RELAY_CONTROLLER_SET_RELAY_3, data);
+		sendAutoDiscoveryMessage(RELAY_CONTROLLER_SET_RELAY_4, data);
+		sendAutoDiscoveryMessage(RELAY_CONTROLLER_SET_RELAY_5, data);
 	}
 }
